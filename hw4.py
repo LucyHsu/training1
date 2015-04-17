98fe175b86bae639f25a14ed6d4ae895cc5fe00c
LucyHsu <lucyhsu@kkbox.com>
2015-04-17 19:34:56 +0800

    use os.walk() instead os.path.isfile()

--- hw4.py
+++ hw4.py
@@ -1,20 +1,20 @@
 import sys
 import os
 
-
+for a,b,file_list in os.walk("."):
+	break
 
 if len(sys.argv) == 1:
 	print "Usage: hw4.py [filename]"
-
-
 
 else:
     file_name = sys.argv[1]
 
     print os.path.isfile('file_name')
-    print 'read file name = ', file_name
+    print 'read file name = ', file_name, '\n'
 
-    if os.path.isfile("file_name"):
+#    if os.path.isfile("./""file_name"):
+    if file_name in file_list:
 	user_file = open(file_name)
         value_in_file = user_file.read()
         print value_in_file
