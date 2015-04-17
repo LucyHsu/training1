9e745a48c5af19d063af0fe80433136d697aed8b
LucyHsu <lucyhsu@kkbox.com>
2015-04-17 18:39:44 +0800

    l

--- hw4.py
+++ hw4.py
@@ -1,6 +1,11 @@
 import sys
+import os
 
-file_name_list = ['sample1.dat','sample3.dat']
+#file_name_list = ['sample1.dat','sample3.dat']
+
+for dirPath, dirNames, fileNames in os.walk("./"):
+    file_name_list = fileNames[0]
+print file_name_list
 
 if len(sys.argv) == 1:
 	print "Usage: hw4.py [filename]"
