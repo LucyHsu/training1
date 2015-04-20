9371cff793271cb651b8ee20cdf66cc6864f7229
LucyHsu <lucyhsu@kkbox.com>
2015-04-20 12:05:33 +0800

    1.use os.path.isfile() istead os.walk() 2.change output style

--- hw4.py
+++ hw4.py
@@ -1,23 +1,18 @@
 import sys
 import os
 
-for a,b,file_list in os.walk("."):
-	break
-
 if len(sys.argv) == 1:
 	print "Usage: hw4.py [filename]"
 
 else:
     file_name = sys.argv[1]
-
-    print os.path.isfile('file_name')
-    print 'read file name = ', file_name, '\n'
-
-#    if os.path.isfile("./""file_name"):
-    if file_name in file_list:
+    if os.path.isfile(file_name):
 	user_file = open(file_name)
-        value_in_file = user_file.read()
-        print value_in_file
+	value_in_file = user_file.read()
+	value_in_file_list = value_in_file.split('\n')
+	cut_extra_item = value_in_file_list.pop()
+	for index, value in enumerate(value_in_file_list):
+            print  index+1, ':', value
 
     else: 
 	print 'File not found.'
